ca46a1a188af820eae0bbdb4330595f2764a416c
mamba <dengzhifeng_63@163.com>
2024-08-27 17:22:10 +0800

    feat(page-api): 优化

---
+++ assets/test.e6c728ca.js
@@ -0,0 +1,1 @@
+import{d as e,o as s,D as t,j as a,h as n}from"./index.44fed5de.js";const o=e({name:"Test",setup:e=>(s((()=>{})),(e,s)=>{const o=t("ContentArea");return n(),a(o)})});export{o as default};
