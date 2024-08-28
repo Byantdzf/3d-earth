cd9e83fab77099589d1399a72661c4d261670df8
mamba <dengzhifeng_63@163.com>
2024-08-28 10:48:10 +0800

    feat(page-api): 优化（配置#HASH）

--- assets/test.ae90a3eb.js
+++
@@ -1,1 +0,0 @@
-import{d as s,o as e,D as t,j as a,h as n}from"./index.5b4822ec.js";const o=s({name:"Test",setup:s=>(e((()=>{})),(s,e)=>{const o=t("ContentArea");return n(),a(o)})});export{o as default};
