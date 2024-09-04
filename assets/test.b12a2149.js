39fe329a491e54c8a6335666db8b037dfff35166
mamba <dengzhifeng_63@163.com>
2024-09-04 16:01:27 +0800

    feat(page-api): 优化

---
+++ assets/test.b12a2149.js
@@ -0,0 +1,1 @@
+import{d as a,o as s,K as t,j as e,h as n}from"./index.8a3a42b1.js";const o=a({name:"Test",setup:a=>(s((()=>{})),(a,s)=>{const o=t("ContentArea");return n(),e(o)})});export{o as default};
